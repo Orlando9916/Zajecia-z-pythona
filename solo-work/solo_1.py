7d79eb6c8ef34e5861461db32e70d916d7a09edc
Krzysztof Szczesny <szczesnykrzysztof99@icloud.com>
2023-04-17 23:29:28 +0200

    Wszystkie zadania od 1 do 9

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,3 +1,91 @@
+# zadanie 1.1
+
+hello = "Hello"
+student = "Ola"
+
+# oczekiwany rezultat: Hello Ola
+# wykorzystaj w princie zmienne hello i student
+print("{h} {s}" .format (h=hello, s=student))
+
+# zadanie 1.2
+
+student = input("Wpisz swoje imie ")
+
+print("Hello " + student)
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+# policz liczbe studentow w tablicy studenci
+# oczekiwany rezultat: Liczba studentow wynosi: 4
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi: ", liczba_studentow)
+
+# zadanie 1.4
+
+studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
+
+for x in studenci:
+    print("Hello ",x)
+# za pomoca petli i print przywitaj sie z kazdym studentem
+# z tabeli studenci osobno
+# oczekiwany rezultat:
+# Hello Ania
+# Hello Kasia
+# Hello Piotr
+# Hello Tomek
+
+# zadanie 1.5
+
+liczba = 3
+potega = 4
+
+wynik = liczba**potega
+
+# oczekiwany rezultat:
+# Wynik wynosi: 81
+print("Wynik wynosi: ", wynik)
+
+# zadanie 1.6
+
+# policz ilosc nawiasow ( w danym ciagu znakow
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
+
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
+
+# zadanie 1.7
+
+# posortuj alfabetycznie (od imienia) studentow
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+lista= studenci.sort()
+# oczekiwany rezultat:
+# Anna Szczesny
+# Barbara Kowalska
+# Jan Niezbedny
+# Tomasz Nijaki
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
+
+# zadanie 1.8
+
+# posortuj alfabetycznie (od nazwiska) studentow
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+studenci.sort(key=lambda x: x.split()[1])
+# oczekiwany rezultat:
+# Barbara Kowalska
+# Jan Niezbedny
+# Tomasz Nijaki
+# Anna Szczesny
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
+
 # zadanie 1.9
 
 # policz wszystkich studentow z tablicy, ktorych nazwisko zaczyna sie na N
