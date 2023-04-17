8042fb7f65028598dadadbdfedebeefb745ddc56
Krzysztof Szczesny <szczesnykrzysztof99@icloud.com>
2023-04-17 23:20:33 +0200

    Dodano zadanie 1.8

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,14 +1,13 @@
-# zadanie 1.7
+# zadanie 1.8
 
-# posortuj alfabetycznie (od imienia) studentow
+# posortuj alfabetycznie (od nazwiska) studentow
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
-lista= studenci.sort()
+studenci.sort(key=lambda x: x.split()[1])
 # oczekiwany rezultat:
-# Anna Szczesny
 # Barbara Kowalska
 # Jan Niezbedny
 # Tomasz Nijaki
+# Anna Szczesny
 print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
-
